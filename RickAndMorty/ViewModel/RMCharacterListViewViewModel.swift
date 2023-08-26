9ad8abe718902b78a0038d6bb4f9ca76b03d4a89
Oleg <o.zhovtanskyi@gmail.com>
2023-08-26 16:40:54 +0200

    Test commit

--- RickAndMorty/ViewModel/RMCharacterListViewViewModel.swift
+++ RickAndMorty/ViewModel/RMCharacterListViewViewModel.swift
@@ -83,6 +83,22 @@ extension RMCharacterListViewViewModel: UICollectionViewDataSource, UICollection
         return cell
     }
     
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        guard kind == UICollectionView.elementKindSectionFooter else {
+            return UICollectionReusableView()
+        }
+        
+        let footer = collectionView.dequeueReusableSupplementaryView(ofKind: kind,
+                                                                     withReuseIdentifier: RMFooterLoadingCollectionReusableView.identifier,
+                                                                     for: indexPath)
+        return footer
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return CGSize(width: collectionView.frame.width,
+                      height: 100)
+    }
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let bounds = UIScreen.main.bounds
         let width = (bounds.width - 30)/2
@@ -99,7 +115,8 @@ extension RMCharacterListViewViewModel: UICollectionViewDataSource, UICollection
 }
 
 
-//MARK: - ScrollView
+// MARK: - Development only
+//Test
 extension RMCharacterListViewViewModel: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         guard shouldShowLoadMoreIndicator else {
